8aff1b900eba21b513fd3f5171c2e4242ae69ac3
Michael Norton <nortonkmichael@gmail.com>
2021-11-06 19:08:23 -0400

    Create sample templates

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,17 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "hello world"
+    user = {'username': 'Michael'}
+    cards = [
+        {
+            'name': 'Black Lotus',
+            'body': 'tap and sac to add 3 mana'
+        },  
+        {
+            'name': 'Phyrexian Altar',
+            'body': 'sac a creature to add 1 colored mana'
+        }]
+    return render_template('index.html', title="title", user=user, cards=cards)
