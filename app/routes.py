474ec0d90b124bd188a510b57b2f88207229116e
Michael Norton <nortonkmichael@gmail.com>
2021-11-06 21:54:37 -0400

    Add login route and make changes to the form templates

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,7 @@
+from werkzeug.utils import redirect
 from app import app
-from flask import render_template
+from flask import render_template, flash, redirect
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -15,3 +17,11 @@ def index():
             'body': 'sac a creature to add 1 colored mana'
         }]
     return render_template('index.html', title="title", user=user, cards=cards)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(f'Login requested for user {form.username.data}, remember_me={form.remember_me.data}') # Posible error here...
+        return redirect('/index')
+    return render_template('login.html', title="Sign In", form=form)
