af0f9addef6faf098c2376baab1ddbeeea966051
Michael Norton <nortonkmichael@gmail.com>
2021-11-10 21:16:33 -0500

    Add user login feature

--- app/models.py
+++ app/models.py
@@ -1,13 +1,23 @@
-from app import db
+from datetime import datetime
+from app import db, login
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
-    def __init__(self, email, password_hash):
+    def __init__(self, email):
         self.email = email
-        self.password_hash = password_hash # I'll remove this and replace with a hashing function soon...
+         # I'll remove this and replace with a hashing function soon...
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
         return f'<User {self.email}>'
@@ -47,3 +57,8 @@ class Card(db.Model):
 
     def __repr__(self):
         return f'<Card {self.name}>'
+
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
