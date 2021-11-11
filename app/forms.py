a5ea144ccf1ac0a6d017f81bd8d9844e079a29ca
Michael Norton <nortonkmichael@gmail.com>
2021-11-10 21:48:42 -0500

    Add user registration

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,22 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
+from app.models import User
 
 class LoginForm(FlaskForm):
-    email = StringField("Email", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired(), Email()])
     password = PasswordField("Password", validators=[DataRequired()])
     remember_me = BooleanField("Remember Me")
     submit = SubmitField('Sign In')
+
+class RegistrationForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = StringField('Password', validators=[DataRequired()])
+    password2 = StringField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+    def validate_email(self, email):            # WTForms automatically adding these methods to the above validations, that's neat!
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('User with that email already exists.')
+
--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,10 @@
 from flask_login.utils import login_required
 from werkzeug.utils import redirect
 from werkzeug.urls import url_parse
-from app import app
+from app import app, db
 from app.models import User
 from flask import render_template, flash, redirect, url_for, request
-from app.forms import LoginForm
+from app.forms import LoginForm, RegistrationForm
 from flask_login import current_user, login_user, logout_user, login_required
 
 @app.route('/')
@@ -43,3 +43,17 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
